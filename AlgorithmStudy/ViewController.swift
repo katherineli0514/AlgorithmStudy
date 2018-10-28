06319af1bbf53992509be35b01ff6d7e2a8d3594
Katherine Li <katherineli0514@gmail.com>
2018-10-27 23:12:56 -0700

    Add removeDuplicates problems to the project.

--- AlgorithmStudy/ViewController.swift
+++ AlgorithmStudy/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  AlgorithmStudy
 //
-//  Created by Han Li on 10/10/18.
+//  Created by Katherine Li on 10/10/18.
 //  Copyright © 2018 katherine. All rights reserved.
 //
 
@@ -12,9 +12,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print(removeDuplicates(array: [12,45,12,55,33,12,45]))
+        print(removeDuplicateInts(array: [12,45,12,55,33,12,45]))
+        
     }
-
+    
+    func removeDuplicates(array: [Int]) -> [Int] {
+        var outArray:[Int] = []
+        for i in 0..<array.count {
+            if !outArray.contains(array[i]) {
+                outArray.append(array[i])
+            }
+        }
+        return outArray
+    }
+    
+    func removeDuplicateInts(array: [Int]) -> [Int] {
+        let uniques = Set<Int>(array)
+        let outArray = Array<Int>(uniques)
+        return outArray
+    }
 
 }
 
